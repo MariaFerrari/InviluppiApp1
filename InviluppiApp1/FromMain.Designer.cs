efb5b5737b6e2a927d7b137a44b57a47cc19eb1e
Maria Ferrari <maria.ferrari.studenti@isii.it>
2020-11-15 16:15:18 +0100

    Aggiungere i file di progetto.

---
+++ InviluppiApp1/FromMain.Designer.cs
@@ -0,0 +1,62 @@
+﻿namespace InviluppiApp1
+{
+    partial class FromMain
+    {
+        /// <summary>
+        /// Variabile di progettazione necessaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Pulire le risorse in uso.
+        /// </summary>
+        /// <param name="disposing">ha valore true se le risorse gestite devono essere eliminate, false in caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Codice generato da Progettazione Windows Form
+
+        /// <summary>
+        /// Metodo necessario per il supporto della finestra di progettazione. Non modificare
+        /// il contenuto del metodo con l'editor di codice.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.inviluppoControl1 = new InviluppiApp1.InviluppoControl();
+            this.SuspendLayout();
+            // 
+            // inviluppoControl1
+            // 
+            this.inviluppoControl1.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.inviluppoControl1.BackColor = System.Drawing.Color.DarkGray;
+            this.inviluppoControl1.Location = new System.Drawing.Point(163, 45);
+            this.inviluppoControl1.N = 10;
+            this.inviluppoControl1.Name = "inviluppoControl1";
+            this.inviluppoControl1.Size = new System.Drawing.Size(445, 445);
+            this.inviluppoControl1.TabIndex = 0;
+            this.inviluppoControl1.Text = "inviluppoControl1";
+            // 
+            // FromMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 514);
+            this.Controls.Add(this.inviluppoControl1);
+            this.Name = "FromMain";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private InviluppoControl inviluppoControl1;
+    }
+}
+
---
+++ InviluppiApp1/InviluppoControl.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace InviluppiApp1
+{
+    public class InviluppoControl : Control
+    {
+        private int _N;
+        public int N
+        {
+            get { return _N; }
+            set
+            {
+                if (value <= 0)
+                    throw new ArgumentOutOfRangeException("N>0");
+                _N = value;
+            }
+        }
+        public InviluppoControl()
+        {
+            _N = 10;
+            base.BackColor = Color.DarkGray;
+        }
+        public InviluppoControl(int n)
+        {
+            N = n;
+            base.BackColor = Color.DarkGray;
+        }
+        public InviluppoControl(int n, Color color)
+        {
+            N = n;
+            base.BackColor = color;
+        }
+        protected override void OnPaint(PaintEventArgs e)
+        {
+            base.OnPaint(e);
+            Graphics g = e.Graphics;
+            Pen p = new Pen(Color.Turquoise);
+          
+
+            for(int i=0; i<Width/2; i += 10)
+            {
+                g.DrawLine(new Pen(Color.DarkTurquoise), 0, i, i, Height/2);
+                //g.DrawLine(new Pen(Color.DarkTurquoise), Width/2, i, Width/2 - i, Height/2);
+                g.DrawLine(new Pen(Color.DarkTurquoise), i, 0, Width/2, i);
+                //g.DrawLine(new Pen(Color.DarkTurquoise), Height/2 - i, 0, 0, i);
+
+                //g.DrawLine(new Pen(Color.DarkTurquoise), 0, i, i, Height / 2);
+                g.DrawLine(new Pen(Color.DarkTurquoise), Width, i, Width - i, Height / 2);
+                //g.DrawLine(new Pen(Color.DarkTurquoise), i, 0, Width / 2, i);
+                g.DrawLine(new Pen(Color.DarkTurquoise), Width - i, 0, Width / 2, i);
+
+                g.DrawLine(new Pen(Color.DarkTurquoise), 0, Height / 2 + i, Width / 2 - i, Height / 2);
+                //g.DrawLine(new Pen(Color.DarkTurquoise), Width/2, i, Width/2 - i, Height/2);
+                g.DrawLine(new Pen(Color.DarkTurquoise), i, Height, Width / 2, Height - 1 - i);
+                //g.DrawLine(new Pen(Color.DarkTurquoise), Height/2 - i, 0, 0, i);
+
+                //g.DrawLine(new Pen(Color.DarkTurquoise), 0, i, i, Height / 2);
+                g.DrawLine(new Pen(Color.DarkTurquoise), Width/2, Height/2 + i, Width/2 + i, Height);
+                //g.DrawLine(new Pen(Color.DarkTurquoise), i, 0, Width / 2, i);
+                g.DrawLine(new Pen(Color.DarkTurquoise), Width - i, Height/2, Width, Height - i);
+                
+                ////////////////////////////////////////////////////////////////
+                
+                //g.DrawLine(new Pen(Color.DarkTurquoise), 0, i, i, Height / 2);
+                g.DrawLine(new Pen(Color.DarkRed), 0, Height - i, Width / 2 - i, Height);
+                //g.DrawLine(new Pen(Color.DarkTurquoise), i, 0, Width / 2, i);
+                g.DrawLine(new Pen(Color.DarkRed), i, Height / 2, Width / 2, Height / 2 + i);
+
+                //g.DrawLine(new Pen(Color.DarkTurquoise), 0, i, i, Height / 2);
+                g.DrawLine(new Pen(Color.DarkRed), Width / 2, i, Width / 2 + i, Height / 2);
+                //g.DrawLine(new Pen(Color.DarkTurquoise), i, 0, Width / 2, i);
+                g.DrawLine(new Pen(Color.DarkRed), Width, Height / 2 - i, Width - i, 0);
+
+            }
+        }
+    }
+}
